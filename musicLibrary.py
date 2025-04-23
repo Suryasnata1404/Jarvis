b95817d4777d8dfafb07274bb58fa047119cb968
Suryasnata Mohapatra <suryasnata2004@gmail.com>
2025-04-23 23:59:27 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+     "ganga":"https://www.youtube.com/shorts/wmitI18zRKI",
+    "namo":"https://www.youtube.com/shorts/piKLtCI_oQo",
+    "krishna": "https://www.youtube.com/shorts/T9scm_OvwiA",
+}
